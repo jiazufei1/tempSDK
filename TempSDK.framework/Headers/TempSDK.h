e6d604b9318e7c350c71a1a5460188192801aac0
曹飞 <blockc@sina.com>
2020-04-14 14:36:57 +0800

    first commit

---
+++ TempSDK.framework/Headers/TempSDK.h
@@ -0,0 +1,20 @@
+//
+//  TempSDK.h
+//  TempSDK
+//
+//  Created by 曹飞 on 2020/4/13.
+//  Copyright © 2020 曹飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TempSDK.
+FOUNDATION_EXPORT double TempSDKVersionNumber;
+
+//! Project version string for TempSDK.
+FOUNDATION_EXPORT const unsigned char TempSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TempSDK/PublicHeader.h>
+
+
+#import <TempSDK/TempSDKTest.h>
---
+++ TempSDK.framework/Headers/TempSDKTest.h
@@ -0,0 +1,18 @@
+//
+//  TempSDKTest.h
+//  TempSDK
+//
+//  Created by 曹飞 on 2020/4/13.
+//  Copyright © 2020 曹飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Masonry/Masonry.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TempSDKTest : NSObject
++(void)test;
+@end
+
+NS_ASSUME_NONNULL_END
